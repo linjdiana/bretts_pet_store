6d2b13c2f175baa2c85dd06fb5bbad204a62a290
Brett de Bear <bkdebear@gmail.com>
2023-03-08 12:30:09 -0800

    troubleshot alembic

---
+++ lib/db/migrations/versions/23d19df891e2_merging_heads.py
@@ -0,0 +1,24 @@
+"""merging heads
+
+Revision ID: 23d19df891e2
+Revises: 191e07adf868, fd6a3d148ea5
+Create Date: 2023-03-08 12:14:36.821778
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '23d19df891e2'
+down_revision = ('191e07adf868', 'fd6a3d148ea5')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
